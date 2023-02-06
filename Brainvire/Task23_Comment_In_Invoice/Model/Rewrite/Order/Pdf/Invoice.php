57ad3908977477fe85f5b557bc515478e448cb24
Ronit Zinzuvadiya <96730598+RonitZinzuvadiya1040@users.noreply.github.com>
2023-02-06 11:47:32 +0530

    Add files via upload

---
+++ Brainvire/Task23_Comment_In_Invoice/Model/Rewrite/Order/Pdf/Invoice.php
@@ -0,0 +1,119 @@
+<?php
+namespace Brainvire\Task23_Comment_In_Invoice\Model\Rewrite\Order\Pdf;
+
+use Magento\Sales\Model\ResourceModel\Order\Invoice\Collection;
+use Magento\Sales\Model\Order\Pdf\AbstractPdf;
+use Magento\Sales\Model\Order\Pdf\Config;
+use Magento\Sales\Model\Order\Pdf\Invoice as MagentoInvoice;
+
+class Invoice extends MagentoInvoice
+{
+
+    protected function _drawHeader(\Zend_Pdf_Page $page)
+    {
+        /* Add table head */
+        $this->_setFontRegular($page, 10);
+        $page->setFillColor(new \Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
+        $page->setLineColor(new \Zend_Pdf_Color_GrayScale(0.5));
+        $page->setLineWidth(0.5);
+        $page->drawRectangle(25, $this->y, 570, $this->y - 15);
+        $this->y -= 10;
+        $page->setFillColor(new \Zend_Pdf_Color_Rgb(0, 0, 0));
+
+        //columns headers
+        $lines[0][] = ['text' => __('Products'), 'feed' => 35];
+
+        $lines[0][] = ['text' => __('SKU'), 'feed' => 290, 'align' => 'right'];
+
+        $lines[0][] = ['text' => __('Qty'), 'feed' => 435, 'align' => 'right'];
+
+        $lines[0][] = ['text' => __('Price'), 'feed' => 375, 'align' => 'right'];
+
+        $lines[0][] = ['text' => __('Tax'), 'feed' => 495, 'align' => 'right'];
+
+        $lines[0][] = ['text' => __('Subtotal'), 'feed' => 565, 'align' => 'right'];
+
+        $lineBlock = ['lines' => $lines, 'height' => 5];
+
+        $this->drawLineBlocks($page, [$lineBlock], ['table_header' => true]);
+        $page->setFillColor(new \Zend_Pdf_Color_GrayScale(0));
+        $this->y -= 20;
+    }
+
+    public function getPdf($invoices = [])
+    {
+        $this->_beforeGetPdf();
+        $this->_initRenderer('invoice');
+
+        $pdf = new \Zend_Pdf();
+        $this->_setPdf($pdf);
+        $style = new \Zend_Pdf_Style();
+        $this->_setFontBold($style, 10);
+
+        foreach ($invoices as $invoice) {
+            if ($invoice->getStoreId()) {
+                $this->_localeResolver->emulate($invoice->getStoreId());
+                $this->_storeManager->setCurrentStore($invoice->getStoreId());
+            }
+
+            $page = $this->newPage();
+            $order = $invoice->getOrder();
+            /* Add image */
+            $this->insertLogo($page, $invoice->getStore());
+            /* Add address */
+            $this->insertAddress($page, $invoice->getStore());
+            /* Add head */
+            $this->insertOrder(
+                $page,
+                $order,
+                $this->_scopeConfig->isSetFlag(
+                    self::XML_PATH_SALES_PDF_INVOICE_PUT_ORDER_ID,
+                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+                    $order->getStoreId()
+                )
+            );
+            /* Add document text and number */
+            $this->insertDocumentNumber($page, __('Invoice # ') . $invoice->getIncrementId());
+            /* Add table */
+            $this->_drawHeader($page);
+            /* Add body */
+            foreach ($invoice->getAllItems() as $item) {
+                if ($item->getOrderItem()->getParentItem()) {
+                    continue;
+                }
+                /* Draw item */
+                $this->_drawItem($item, $page, $order);
+                $page = end($pdf->pages);
+            }
+            /* Add totals */
+            $this->insertTotals($page, $invoice);
+            if ($invoice->getStoreId()) {
+                $this->_localeResolver->revert();
+            }
+
+            foreach ($invoice->getOrder()->getStatusHistoryCollection() as $status){
+                $textChunk = wordwrap(strip_tags($status->getComment()), 120, "\n");
+                foreach(explode("\n", $textChunk) as $textLine){
+                    if ($textLine!=='') {
+                        $page->drawText(strip_tags(ltrim($textLine)), 35, $this->y, 'UTF-8');
+                        $this->y -= 15;
+                    }
+                }
+            }
+        }
+        $this->_afterGetPdf();
+        return $pdf;
+    }
+
+    public function newPage(array $settings = [])
+    {
+        /* Add new table head */
+        $page = $this->_getPdf()->newPage(\Zend_Pdf_Page::SIZE_A4);
+        $this->_getPdf()->pages[] = $page;
+        $this->y = 800;
+        if (!empty($settings['table_header'])) {
+            $this->_drawHeader($page);
+        }
+        return $page;
+    }
+}
---
+++ Brainvire/Task23_Comment_In_Invoice/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'Brainvire_Task23_Comment_In_Invoice',
+	__DIR__
+);
