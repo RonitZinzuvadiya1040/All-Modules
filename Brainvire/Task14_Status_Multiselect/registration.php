a7751630659ae6221196c1f1e07f54bba8532247
Ronit Zinzuvadiya <96730598+RonitZinzuvadiya1040@users.noreply.github.com>
2023-02-06 11:46:05 +0530

    Add files via upload

---
+++ Brainvire/Task14_Status_Multiselect/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+	ComponentRegistrar::MODULE,
+	'Brainvire_Task14_Status_Multiselect',
+	__DIR__
+);
