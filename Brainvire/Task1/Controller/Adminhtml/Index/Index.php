46795ae633a037aa4b9bccd0dee7ce13c1b1049c
Ronit Zinzuvadiya <96730598+RonitZinzuvadiya1040@users.noreply.github.com>
2023-02-08 16:15:15 +0530

    Add files via upload

---
+++ Brainvire/Task1/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Brainvire\Task1\Controller\Adminhtml\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action {
+	protected $_resultPageFactory;
+
+	public function __construct(
+		Context $context,
+		PageFactory $resultPageFactory
+	) {
+		$this->_resultPageFactory = $resultPageFactory;
+		parent::__construct($context);
+	}
+
+	public function execute() {
+		echo "Hello";
+		die();
+		$resultPage = $this->_resultPageFactory->create();
+		return $resultPage;
+
+	}
+}
