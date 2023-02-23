b9f0bcc3c7d09221ce3f7e8a2a20cb5422a7e56c
Ronit Zinzuvadiya <96730598+RonitZinzuvadiya1040@users.noreply.github.com>
2023-02-23 18:52:41 +0530

    Delete Brainvire/Deliverydate directory

--- Brainvire/Deliverydate/view/frontend/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    "map": {
-        "*": {
-            "Magento_Checkout/js/model/shipping-save-processor/default" : "Brainvire_Deliverydate/js/shipping-save-processor-default-override"
-        }
-    }
-};
